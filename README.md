a374d9ff54d7d0700243c9ca7817bb2995ee7f9a
Nguyen Duc Tuan <ngductuan203@gmail.com>
2025-03-01 00:32:16 +0700

    config: init setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_application_1
+# EcoChatBot
 
 A new Flutter project.
 
---
+++ lib/routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  // // Introduction
+  // SplashView.routeName: (context) => const SplashView(),
+  // IntroView.routeName: (context) => const IntroView(),
+
+  // // Authentication
+  // SignInView.routeName: (context) => const SignInView(),
+  // SignUpView.routeName: (context) => const SignUpView(),
+  // ResetPasswordView.routeName: (context) => const ResetPasswordView(),
+
+  // // Home
+  // HomeView.routeName: (context) => const HomeView(),
+
+  // // Topic
+  // SelectTopicView.routeName: (context) => SelectTopicView(),
+  // SearchTopicView.routeName: (context) => const SearchTopicView(),
+  // TopicDetailsView.routeName: (context) => const TopicDetailsView(),
+
+  // // Favorite
+  // FavoriteView.routeName: (context) => FavoriteView(),
+  // // Connect teacher
+  // ConnectTeacherView.routeName: (context) => const ConnectTeacherView(),
+  // HangUpMeetingView.routeName: (context) => const HangUpMeetingView()
+};
