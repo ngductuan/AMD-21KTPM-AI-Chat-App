820907a421984a5422c2090ffbe526d9777f168e
PG47 <86549123+PG47@users.noreply.github.com>
2025-03-09 14:53:32 +0700

    fix merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:eco_chat_bot/routes.dart';
 import 'package:eco_chat_bot/src/helpers/local_storage_helper.dart';
 import 'package:eco_chat_bot/src/pages/general/views/home.dart';
-import 'package:eco_chat_bot/src/pages/general/home.dart';
 import 'package:eco_chat_bot/src/pages/prompt/prompt_libary.dart';
 
 import 'package:flutter/material.dart';
