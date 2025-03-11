03ae8d51905556e62c786378545065ecab10d1c4
PG47 <86549123+PG47@users.noreply.github.com>
2025-03-12 00:17:03 +0700

    profile Route

--- lib/src/pages/profile/views/profile.dart
+++ lib/src/pages/profile/views/profile.dart
@@ -4,6 +4,8 @@ import '../../../constants/styles.dart';
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
+  static const String routeName = '/profile';
+
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
